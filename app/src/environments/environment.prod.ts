146bf1d0e77f6e1f9c14e0da49e8efaaeede5a76
Patrick Souza Martins <patrick.sm@hotmail.com.br>
2022-11-18 09:40:50 -0300

    Ajuste nas configurações

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	urlApiBase: "https://apontamento.azurewebsites.net/api/",
+	urlApiBase: "https://apontamento-api.azurewebsites.net/api/",
 	urlTfs: "http://tfs.vixteam.com.br:8080/tfs/",
 	urlGraphAzure: "https://graph.microsoft.com/v1.0/me/",
 	chaveStorageToken: "8e294461-b28a-4221-a1c6-f1a246df5cb6",
@@ -8,7 +8,7 @@ export const environment = {
 	chaveStorageTarefasFixadas: "afd2c0d0-e22b-4995-a35f-b7dedc150134",
 	chaveStorageConfiguracoes: "81684d56-48db-4317-ac66-515cf96e0a1f",
 	azureAd: {
-		returnUrl: "https://apontamento.azurewebsites.net/",
+		returnUrl: "https://apontamento-vixteam.azurewebsites.net/",
 		authority: "https://login.microsoftonline.com/organizations/",
 		clientId: "ae7b62cf-5a6d-4f4f-9d66-f0596be9ca37"
 	}
