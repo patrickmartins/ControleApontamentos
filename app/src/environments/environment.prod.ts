389a22ad28a1d6ef9ba17fc51a032a2cb7312b80
Patrick Souza Martins <patrick.sm@hotmail.com.br>
2022-11-18 00:11:42 -0300

    Ajuste de configuração da aplicação Angular

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,8 +1,12 @@
 export const environment = {
-	production: true,
+	production: false,
 	urlApiBase: "https://apontamento.azurewebsites.net/api/",
 	urlTfs: "http://tfs.vixteam.com.br:8080/tfs/",
 	urlGraphAzure: "https://graph.microsoft.com/v1.0/me/",
+	chaveStorageToken: "8e294461-b28a-4221-a1c6-f1a246df5cb6",
+	chaveStorageUsuario: "20c919d4-37ad-4311-9a54-c262ba5c146e",
+	chaveStorageTarefasFixadas: "afd2c0d0-e22b-4995-a35f-b7dedc150134",
+	chaveStorageConfiguracoes: "81684d56-48db-4317-ac66-515cf96e0a1f",
 	azureAd: {
 		returnUrl: "https://apontamento.azurewebsites.net/",
 		authority: "https://login.microsoftonline.com/organizations/",
