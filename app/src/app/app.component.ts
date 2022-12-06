266814230a229481c27b1b8ff104dc3ea31638a2
Patrick Souza Martins <patrick.sm@hotmail.com.br>
2022-12-06 09:45:34 -0300

    Código temporário para remoção de tokens antigos

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { TemaService } from './core/services/tema.service';
 
 @Component({
@@ -11,5 +12,20 @@ export class AppComponent {
 
 	constructor(private temaService: TemaService) {
 		this.temaService.aplicarTemaAtual();
+		this.removerTokensAntigos();
 	}	
+
+	public removerTokensAntigos() {
+		let valor = localStorage.getItem(environment.chaveStorageToken);
+
+		if(!valor)
+			return;
+
+		let token = JSON.parse(valor);
+
+		if(typeof token.validade === 'number') {
+			localStorage.removeItem(environment.chaveStorageToken);
+			localStorage.removeItem(environment.chaveStorageUsuario);
+		}
+	}
 }
